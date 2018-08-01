81e2d1a6b05fe207f07b65098c97d66a17a46028
JLasc <lascaze.j@gmail.com>
2018-08-01 04:38:52 -0400

    message changes

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
     
   var i = 0;
-  var txt = `Welcome to my website! I'm currently putting it all together. Please come back soon!`; /* The text */
+  var txt = `Welcome to my portfolio page. I'm currently putting it all together. Please come back soon !`; /* The text */
   var speed = 100; /* The speed/duration of the effect in milliseconds */
 
   function typeWriter() {
