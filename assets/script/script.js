3c99e8a15e843fd5da19ab10cb6c4b275e5ebafb
JLasc <lascaze.j@gmail.com>
2018-08-04 05:30:17 -0400

    remove typewriter, add translate animation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,25 +1,73 @@
+
 $(document).ready(function(){
+  var selfGif = $("#test-image");
+  $('.materialboxed').materialbox();
+  $('.parallax').parallax();
+
+ function start() {
+    $("#self-gif")
+      .attr("src", "./assets/img/Type.gif")
+      .addClass("animated zoomIn");
+
+  };
+
+  start()
+
+  function empty() {
+    $("#start-container").empty()
+  }
+
+  function startFadeOut() {
+    $("#self-gif")
+      .removeClass("animated fadeIn")
+      .addClass("animated zoomOut");
+      setTimeout(empty, 500)
+  } 
+
+  function reveal() {
+    $("#page-container")
+      .removeClass("hidden")
+      .addClass("animated zoomIn")
+    $("#page-footer")
+      .removeClass("hidden")
+      .addClass("animated zoomIn")
+  }
 
 
-    
-  var i = 0;
-  var txt = `Welcome to my portfolio page. I'm currently putting it all together. Please come back soon !`; /* The text */
-  var speed = 100; /* The speed/duration of the effect in milliseconds */
+setTimeout(startFadeOut, 1000 * 4)
+setTimeout(reveal, 1000 * 5)
 
-  function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("demo").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
+// Wrap every letter in a span
+$('.ml12').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: function(el, i) {
+      return 500 + 30 * i;
     }
-}
+  }).add({
+    targets: '.ml12 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: function(el, i) {
+      return 100 + 30 * i;
+    }
+  });
 
 
 
 
-typeWriter()
-
-
+  
   });
 
   
