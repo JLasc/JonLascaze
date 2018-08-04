fd897d56b39e078b8fc2b219dd8de59bdcadbcd1
JLasc <lascaze.j@gmail.com>
2018-08-04 05:32:25 -0400

    script changes

--- assets/script/script.js
+++ assets/script/script.js
@@ -42,7 +42,7 @@ $('.ml12').each(function(){
   $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
 
-anime.timeline({loop: true})
+anime.timeline({loop: false})
   .add({
     targets: '.ml12 .letter',
     translateX: [40,0],
