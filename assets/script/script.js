f4d6833ad110b85750001224ac9f2afdc7d72ea4
JLasc <lascaze.j@gmail.com>
2018-08-04 22:01:07 -0400

    button events that dynamically change page

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,16 @@
 
 $(document).ready(function(){
-  var selfGif = $("#test-image");
+  
+  //Materialize Init
   $('.materialboxed').materialbox();
   $('.parallax').parallax();
 
+
+  //Starting Logo Animation
  function start() {
     $("#self-gif")
       .attr("src", "./assets/img/Type.gif")
       .addClass("animated zoomIn");
-
   };
 
   start()
@@ -24,18 +26,26 @@ $(document).ready(function(){
       setTimeout(empty, 500)
   } 
 
+
+  //Reveals main cards
   function reveal() {
-    $("#page-container")
-      .removeClass("hidden")
-      .addClass("animated zoomIn")
-    $("#page-footer")
-      .removeClass("hidden")
-      .addClass("animated zoomIn")
+    $("#page-container").removeClass("hidden")
+      .fadeOut(.05, function() {
+         $("#page-container").fadeIn(1500)
+      })
+    $("#name-text").removeClass("hidden")
+      .fadeOut(.05, function() {
+         $("#name-text").fadeIn(1500)
+      })
+    $("#page-footer").removeClass("hidden")
+      .fadeOut(.05, function() {
+         $("#page-footer").fadeIn(1500)
+      })
   }
 
 
-setTimeout(startFadeOut, 1000 * 4)
-setTimeout(reveal, 1000 * 5)
+setTimeout(startFadeOut, 1000 * 3.5)
+setTimeout(reveal, 1000 * 4)
 
 // Wrap every letter in a span
 $('.ml12').each(function(){
@@ -63,6 +73,125 @@ anime.timeline({loop: false})
       return 100 + 30 * i;
     }
   });
+
+
+  //About Me - grow 
+  $( "#aboutme" ).click(function() {
+    $("#applications")
+        .slideUp(1500, function() {
+          $("#applications").addClass("hidden-button")
+        });
+    $("#website")
+        .slideUp(1500, function() {
+          $("#website").addClass("hidden-button")
+        });
+    $( "#aboutme" ).empty().animate({
+      height: "700px",
+    }, 1500, function() {
+      $("#aboutme")
+      .removeClass("white-text")
+      .addClass("about-me black-text")
+      .removeAttr("id", "aboutme")
+      .attr("id", "newMe");
+
+      var aboutSection = 
+      `
+      <div class="container">
+      <div class="row">
+      <img class="left self-picture" src="./assets/img/me.jpg">
+      <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur hendrerit tellus a turpis euismod porta. Phasellus efficitur lorem et dui luctus, in hendrerit dui congue. Suspendisse varius ultricies metus, ut venenatis nibh pulvinar ac. Suspendisse leo sem, dignissim eget felis sit amet, semper dictum ipsum. Praesent non aliquam neque, a fermentum sapien. Sed at ligula et mauris tristique laoreet. Duis sit amet justo eu turpis finibus dignissim at sed eros. Nulla interdum dolor neque, et elementum lorem egestas non. Praesent blandit lectus augue, vel pretium neque fringilla vel. Cras cursus felis ut ligula volutpat, ut condimentum massa mollis. Etiam erat erat, dictum tristique mauris sit amet, ullamcorper lacinia leo. Nullam at enim dapibus, tempor nibh a, dignissim erat. Praesent fermentum rhoncus quam at varius. Nunc bibendum urna vitae tempor vehicula. Integer faucibus mauris ut lacus consequat aliquet.</p>
+      </div>
+      <div class="row center-align">
+      <ul>
+      <li><i class="fab fa-linkedin-in"></i> LinkedIn</li>
+      <li><i class="fab fa-github-alt"></i> Github</li>
+      <li><i class="fab fa-stack-overflow"></i> Stack Overflow</li>
+      <li><i class="far fa-envelope"></i> Email</li>
+      <li><i class="fas fa-file-pdf"></i> Resume</li>
+      </ul>
+      </div>
+      </div>
+      <a class="waves-effect waves-light pulse btn black" id="back-about-me">Back</a>
+      `
+      $("#newMe").append(aboutSection);
+    });
+  });
+
+
+  //About Me - shrink
+  $(document).on("click", "#back-about-me", function(){
+    $("#newMe")
+    .empty()
+    .removeClass("about-me black-text")
+    .removeAttr("id", "newMe")
+    .attr("id", "aboutme");
+
+    $("#aboutme")
+      .append("<p>About</p>")
+      .animate({
+          height: "250px",
+        }, 750, function() {
+          $("#aboutme")
+            .removeAttr("style")
+            .addClass("white-text");
+            $("#applications").slideDown(1500, function() {
+              $("#applications").removeClass("hidden-button");
+            });
+            $("#website").slideDown(1500, function() {
+              $("#website").removeClass("hidden-button")
+            });
+          });
+  });
+
+
+
+
+
+// Application - grow
+  $("#applications").on("click", function() {
+    
+    $("#aboutme").slideUp(1500, function() {
+      $("#aboutme").addClass("hidden-button")
+      $("#applications").animate({
+        height: "700px",
+      }, 1500, function() {
+        $("#applications").empty()
+        .removeClass("white-text")
+        .addClass("about-me black-text")
+        .removeAttr("id", "applications")
+        .attr("id", "newApp"); 
+
+      var appSection = 
+      `
+      <div class="container">
+      <div class="row">
+      <img class="left self-picture" src="./assets/img/me.jpg">
+      <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur hendrerit tellus a turpis euismod porta. Phasellus efficitur lorem et dui luctus, in hendrerit dui congue. Suspendisse varius ultricies metus, ut venenatis nibh pulvinar ac. Suspendisse leo sem, dignissim eget felis sit amet, semper dictum ipsum. Praesent non aliquam neque, a fermentum sapien. Sed at ligula et mauris tristique laoreet. Duis sit amet justo eu turpis finibus dignissim at sed eros. Nulla interdum dolor neque, et elementum lorem egestas non. Praesent blandit lectus augue, vel pretium neque fringilla vel. Cras cursus felis ut ligula volutpat, ut condimentum massa mollis. Etiam erat erat, dictum tristique mauris sit amet, ullamcorper lacinia leo. Nullam at enim dapibus, tempor nibh a, dignissim erat. Praesent fermentum rhoncus quam at varius. Nunc bibendum urna vitae tempor vehicula. Integer faucibus mauris ut lacus consequat aliquet.</p>
+      </div>
+      <div class="row center-align">
+      <ul>
+      <li><i class="fab fa-linkedin-in"></i> LinkedIn</li>
+      <li><i class="fab fa-github-alt"></i> Github</li>
+      <li><i class="fab fa-stack-overflow"></i> Stack Overflow</li>
+      <li><i class="far fa-envelope"></i> Email</li>
+      <li><i class="fas fa-file-pdf"></i> Resume</li>
+      </ul>
+      </div>
+      </div>
+      <a class="waves-effect waves-light pulse btn black" id="back-about-me">Back</a>
+      `
+      $("#newApp").append(appSection);
+      });
+    });
+
+    $("#website").fadeOut(1500, function() {
+      $("#website").addClass("hidden-button")
+    });
+
+    
+  });
+
+
 
 
 
